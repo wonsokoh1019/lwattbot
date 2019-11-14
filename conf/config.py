d4421b7356e833832ea763953fb27227f0b59051
wonsokoh1019 <wonsok.oh@worksmobile.com>
2019-11-14 14:22:17 +0900

    dd
    dd

--- conf/config.py
+++ conf/config.py
@@ -4,26 +4,26 @@ import os
 
 # account
 LANG = "kr"    # ["kr"|"en"|"jp"]
-ADMIN_ACCOUNT = "admin@lwbottest"
-RECEIVE_ACCOUNT = "admin02@nwetest.com"
-DOMAIN_ID = 18644
+ADMIN_ACCOUNT = "admin@lwbottest02"
+RECEIVE_ACCOUNT = "admin@lwbottest02"
+DOMAIN_ID = 10123847
 
 # api
-API_ID = "kr1EHAIjvfJVz"
-CONSUMER_KEY = "To8SnC7sLIAv8GjqXZhO"
+API_ID = "jp1HOduOeHyIK"
+CONSUMER_KEY = "4d66rICCBuJtLIo5XYgf"
 
 TOKEN = None
-SERVER_ID = "96460cc1e778402dae5bfe35fa97ce76"
-PRIVATE_KEY_NAME = "private_20191017164308.key"
+SERVER_ID = "lwtest"
+PRIVATE_KEY_NAME = "private_20191114141800.key"
 
 # LOCAL ADDRESS
 # LOCAL_ADDRESS = "http://10.105.180.133:8080/"
-LOCAL_ADDRESS = "https://10.105.180.133:8080/"
+LOCAL_ADDRESS = "https://lwattbot.herokuapp.com/"
 
 # DB config
-DB_HOST = "10.106.151.241"
+DB_HOST = "ec2-54-83-9-169.compute-1.amazonaws.com"
 DB_PORT = "5432"
-DB_NAME = "dbwyc"
-DB_USER = "dbtest"
-DB_PASSWORD = "123456"
-DB_SSLMODE = "prefer"
+DB_NAME = "df7bekr3hp8vsi"
+DB_USER = "wapkufxqibhiwq"
+DB_PASSWORD = "a36130bebd0086bc5d6c9d08d4638137bbd296af9ef175d931a65e52c9d29693"
+DB_SSLMODE = "require"
